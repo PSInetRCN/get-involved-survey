9c5fb2c11b2aa161665cfff05d27cb1ad80fd7d9
Jessica Guo <jessicaguo@email.arizona.edu>
2023-07-27 16:24:02 -0700

    Change color

--- ui.R
+++ ui.R
@@ -4,5 +4,5 @@ fluidPage(
   surveyOutput(df = df,
                survey_title = "Register",
                survey_description = h4(HTML("Thank you for your interest in joining to PSInet. Please complete the following form, and we look forward to meeting you at a future PSInet event!")),
-               theme = "#348DBC")
+               theme = "#8b668b")
 )
