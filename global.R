9f3670849656f587ce9b990f9e6b3ac6707e8a64
Jessica Guo <jessicaguo@email.arizona.edu>
2023-07-31 09:27:01 -0700

    Fix to PR version of shinysurveys, update (optional) and submit message

--- global.R
+++ global.R
@@ -2,6 +2,9 @@ library(readr)
 library(dplyr)
 library(shiny)
 library(shinysurveys)
+# Make sure to install from a PR
+# remotes::install_github("jdtrat/shinysurveys#43")
+# See SO: https://stackoverflow.com/questions/71512952/is-there-any-other-way-to-create-dependent-survey-questionnaire-using-r-shiny
 library(googlesheets4)
 library(bslib)
 
--- server.R
+++ server.R
@@ -3,29 +3,29 @@ function(input, output, session) {
   renderSurvey()
   
   observeEvent(input$submit, {
-    
+
     gs4_auth(
       cache = gargle::gargle_oauth_cache(),
       email = gargle::gargle_oauth_email()
     )
-    
+
     # Retrieve existing datasheet
-    sheet_url <- "https://docs.google.com/spreadsheets/d/1F7DB2WZDnbTZ48SZxyZDKohc_U1ZimIiEMO_5wX9Qcw/edit#gid=0"
+    sheet_url <- "https://docs.google.com/spreadsheets/d/1TZh0P8gsjvq572LzI0rWTikrvplyvMYVH8rpOJ7fz30/edit#gid=0"
     previous <- read_sheet(sheet_url)
-    
+
     # Obtain and and append submitted results
     response <- getSurveyData(custom_id = input$email,
                               include_dependencies = FALSE)
     updated <- bind_rows(previous, response)
-    
+
     # Write back to Google sheet
     write_sheet(updated, ss = sheet_url, sheet = 'Sheet1')
-    
+
     # Show submission message
     showModal(modalDialog(
-      title = "Thank you for your interest in PSInet.",
+      title = "Your answers have been recorded. Thank you!",
       "Please reach out to Kim Novick (knovick@indiana.edu) or Jessica Guo (jessicaguo@arizona.edu) with any questions. "
     ))
   })
-  
+
 }
